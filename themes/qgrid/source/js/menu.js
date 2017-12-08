9fc12c1f28fe21db8732383f47a9f786108cf58b
Ekaterina Tokareva <Tokaree@labcorp.com>
2017-12-08 20:17:52 +0300

    init

---
+++ themes/qgrid/source/js/menu.js
@@ -0,0 +1,8 @@
+function open() {
+	// document.getElementById("sidenav").style.width = "250px";
+}
+
+function close() {
+	// document.getElementById("sidenav").style.width = "0";
+	console.log('Kate');
+}
