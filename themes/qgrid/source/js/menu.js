ed566aeb686b6cb13ba9985c4739530499ffa0c1
etokareva <etokareva1990@gmail.com>
2017-12-15 14:36:05 +0300

    menu

--- themes/qgrid/source/js/menu.js
+++ themes/qgrid/source/js/menu.js
@@ -1,15 +1,20 @@
-function toggleNav () {
+function toggleDisplay () {
 	var nav = document.getElementById("nav");
-	nav.classList.toggle('hide');
-	var overlay = document.getElementById("overlay");
-	overlay.classList.toggle('hide');
+    var overlay = document.getElementById("overlay");
+	if (nav.style.display==='block'){
+        nav.style.display='none';
+        overlay.style.display='none';
+	} else {
+        nav.style.display='block';
+        overlay.style.display='block';
+    }
 }
 
 function init() {
 
 	const triggerBtns = document.getElementsByClassName("nav-trigger");
 	for (var i = 0; i < triggerBtns.length; i++) {
-		triggerBtns[i].addEventListener('click', toggleNav, false);
+		triggerBtns[i].addEventListener('click', toggleDisplay, false);
 	}
 
 }
