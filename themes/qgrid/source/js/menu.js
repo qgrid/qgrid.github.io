aa61f48ef7a03f2dc188a9d428a7333af5e35d1b
etokareva <etokareva1990@gmail.com>
2017-12-16 20:38:32 +0300

    search init

--- themes/qgrid/source/js/menu.js
+++ themes/qgrid/source/js/menu.js
@@ -9,13 +9,31 @@ function toggleDisplay () {
         overlay.style.display='block';
     }
 }
+function filter() {
+    var input = document.getElementById('search');
+    var filter = input.value.toUpperCase();
+    var ul = document.getElementById("myUL");
+    var li = ul.getElementsByTagName('li');
+
+    for (var i = 0; i < li.length; i++) {
+        var a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
 
 function init() {
 
-	const triggerBtns = document.getElementsByClassName("nav-trigger");
+	var triggerBtns = document.getElementsByClassName("nav-trigger");
 	for (var i = 0; i < triggerBtns.length; i++) {
 		triggerBtns[i].addEventListener('click', toggleDisplay, false);
 	}
+
+    var search = document.getElementById('search');
+		search.addEventListener("keyup", filter, false);
 
 }
 
