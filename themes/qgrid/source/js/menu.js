5e8f85a1e894e62b0b4f8a66aa2e6d0c77f19bca
etokareva <etokareva1990@gmail.com>
2017-12-18 09:38:25 +0300

    search init

--- themes/qgrid/source/js/menu.js
+++ themes/qgrid/source/js/menu.js
@@ -1,23 +1,24 @@
-function toggleDisplay () {
-	var nav = document.getElementById("nav");
+function toggleDisplay() {
+    var nav = document.getElementById("nav");
     var overlay = document.getElementById("overlay");
-	if (nav.style.display==='block'){
-        nav.style.display='none';
-        overlay.style.display='none';
-	} else {
-        nav.style.display='block';
-        overlay.style.display='block';
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+        overlay.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+        overlay.style.display = 'block';
     }
 }
+
 function filter() {
     var input = document.getElementById('search');
-    var filter = input.value.toUpperCase();
-    var ul = document.getElementById("myUL");
-    var li = ul.getElementsByTagName('li');
+    var filter = input.value.toLowerCase();
+    var nav = document.getElementById("nav");
+    var li = nav.getElementsByTagName('li');
 
     for (var i = 0; i < li.length; i++) {
         var a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
@@ -27,13 +28,15 @@ function filter() {
 
 function init() {
 
-	var triggerBtns = document.getElementsByClassName("nav-trigger");
-	for (var i = 0; i < triggerBtns.length; i++) {
-		triggerBtns[i].addEventListener('click', toggleDisplay, false);
-	}
+    var triggerBtns = document.getElementsByClassName("nav-trigger");
+    for (var i = 0; i < triggerBtns.length; i++) {
+        triggerBtns[i].addEventListener('click', toggleDisplay, false);
+    }
 
     var search = document.getElementById('search');
-		search.addEventListener("keyup", filter, false);
+    if (search) {
+        search.addEventListener("keyup", filter, false)
+    }
 
 }
 
