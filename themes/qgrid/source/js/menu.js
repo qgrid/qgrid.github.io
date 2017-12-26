84186b7bf2d64288636a98f7ec9454d37b0b39fa
Ekaterina Tokareva <Tokaree@labcorp.com>
2017-12-26 18:31:10 +0300

    connect two searches

--- themes/qgrid/source/js/menu.js
+++ themes/qgrid/source/js/menu.js
@@ -4,8 +4,8 @@ function toggleVisibility(e) {
 		e.stopImmediatePropagation();
 		return;
 	}
-	var nav = document.getElementById("nav");
-	var overlay = document.getElementById("overlay");
+	var nav = document.getElementById('nav');
+	var overlay = document.getElementById('overlay');
 	if (nav.style.display === 'block') {
 		nav.style.display = 'none';
 		overlay.style.display = 'none';
@@ -17,39 +17,42 @@ function toggleVisibility(e) {
 
 function filter(e) {
 	e.stopImmediatePropagation();
+	activateSearch();
 	var input = e.target || e.srcElement;
 	var filter = input.value.toLowerCase();
-	var nav = document.getElementById("nav");
+	var nav = document.getElementById('nav');
 	var li = nav.getElementsByTagName('li');
 
 	for (var i = 0; i < li.length; i++) {
-		var a = li[i].getElementsByTagName("a")[0];
+		var a = li[i].getElementsByTagName('a')[0];
 		if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
-			li[i].style.display = "";
+			li[i].style.display = '';
 		} else {
-			li[i].style.display = "none";
+			li[i].style.display = 'none';
 		}
 	}
 }
 
-function showSearch(e) {
-	var target = e.target || e.srcElement;
-	target.style.display = "none";
-	var header = document.getElementsByTagName("header")[0];
-	var search = header.getElementsByClassName('search')[0];
-	search.style.display = "";
+function activateSearch() {
+	var searchTrigger = document.getElementById('search-trigger');
+	if (!searchTrigger.classList.contains('active')) {
+		searchTrigger.classList.add('active');
+	}
 }
 
 function updateSearch(e) {
 	var search = document.getElementById('search');
 	var searchFields = document.getElementsByClassName('search');
 	var searchFieldActive = e.target || e.srcElement;
+	var value = searchFieldActive.value;
 	for (var i = 0; i < searchFields.length; i++) {
 		if (searchFields[i] !== searchFieldActive) {
-			searchFields[i].value = searchFieldActive.value;
+			searchFields[i].value = value;
+			searchFields[i].setAttribute('value', value);
 		}
 	}
-	search.value = searchFieldActive.value;
+	search.value = value;
+
 	var event = new Event('searchEvent');
 	search.dispatchEvent(event);
 }
@@ -57,16 +60,16 @@ function updateSearch(e) {
 function init() {
 	var search = document.getElementById('search');
 	if (search) {
-		search.addEventListener("searchEvent", filter, false);
+		search.addEventListener('searchEvent', filter, false);
 	}
-	var navTrigger = document.getElementById("nav-trigger");
+	var navTrigger = document.getElementById('nav-trigger');
 	if (navTrigger) {
 		navTrigger.addEventListener('click', toggleVisibility, true);
 	}
-	var overlay = document.getElementById("overlay");
+	var overlay = document.getElementById('overlay');
 	overlay.addEventListener('click', toggleVisibility, true);
 
-	var nav = document.getElementById("nav");
+	var nav = document.getElementById('nav');
 	if (nav) {
 		nav.addEventListener('click', toggleVisibility, true);
 	}
@@ -74,14 +77,14 @@ function init() {
 	var searchFields = document.getElementsByClassName('search');
 	if (searchFields.length) {
 		for (var i = 0; i < searchFields.length; i++) {
-			searchFields[i].addEventListener("keyup", updateSearch, true);
+			searchFields[i].addEventListener('keyup', updateSearch, true);
 		}
 	}
 
 	var searchTrigger = document.getElementById('search-trigger');
 	if (searchTrigger) {
-		searchTrigger.addEventListener("click", showSearch, true);
+		searchTrigger.addEventListener('click', activateSearch, true);
 	}
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init);
