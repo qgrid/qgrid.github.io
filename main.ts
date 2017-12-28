e84835bd1f3963dd1a34538c90783272f97beeac
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-28 23:35:43 +0300

    added run method

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
+import { NgModule, Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
@@ -9,8 +9,8 @@ import { AppComponent } from './app.component';
 import { GridModule } from 'ng2-qgrid';
 import { ThemeModule } from 'ng2-qgrid';
 
-const wnd = window as any;
-wnd.runExample = function run(Component) {
+
+function run(Component) {
   @NgModule({
     imports: [
       BrowserModule,
@@ -28,3 +28,8 @@ wnd.runExample = function run(Component) {
 
   platformBrowserDynamic().bootstrapModule(ExampleModule);
 }
+
+window.ng = {
+  run,
+  Component
+};
