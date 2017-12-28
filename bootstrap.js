66e5a90423b1b7b48e0f1536ee6648873c16d7c4
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-28 23:16:00 +0300

    fix version

--- bootstrap.js
+++ bootstrap.js
@@ -1,1 +1,1 @@
-System.import('https://rawgit.com/qgrid/doc/v1.0.1/main.ts').catch(console.error.bind(console));
+System.import('https://rawgit.com/qgrid/doc/v1.0.2/main.ts').catch(console.error.bind(console));
