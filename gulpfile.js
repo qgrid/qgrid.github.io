42c0002412a03ae837bb36d23d6b426ef9c500e9
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-29 14:44:49 +0300

    add qgrid example

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,17 @@ const gulp = require("gulp");
 const unzip = require("gulp-unzip");
 const path = require("path");
 const minimist = require("minimist");
-
-var knownOptions = {
-  string: "version",
-  default: { version: "1.0.0" }
-};
+const minimatch = require("minimatch");
 
 const options = minimist(process.argv.slice(2));
 
 gulp.task("unzip", () => {
   return gulp
     .src("ng/*.{tar,tar.bz2,tar.gz,zip}")
-    .pipe(unzip())
+    .pipe(
+      unzip({
+        filter: entry => minimatch(entry.path, "**/*.ts")
+      })
+    )
     .pipe(gulp.dest(path.join("dist", options.version)));
 });
