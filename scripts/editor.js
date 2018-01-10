01b8b3b68176e175092f991109256d06b179fd2f
klumba12 <dimitry.sitnov@gmail.com>
2018-01-11 00:24:11 +0300

    add tabs

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,9 +1,22 @@
 const helper = args => {
-  return `<span class="sb-embed">
-            <iframe src="//stackblitz.com/edit/angular-qgrid-example?embed=1&file=app/app.component.html&hideExplorer=1&hideNavigation=1&view=preview"></iframe>
-          </span>
-        `;
+  const name = args[0];
+  const file = "app/app.component.html";
+  console.log(name);
+  return `
+  <div class="editor">
+    <ul class="show">
+      <li class="active">Preview</li>
+      <li>HTML</li>
+      <li>TypeScript</li>
+    </ul>
+    <span class="preview">
+      <iframe src="//stackblitz.com/edit/${name}?embed=1&file=${file}&hideExplorer=1&hideNavigation=1&view=preview"></iframe>
+    </span>
+    <span class="html">
+    </span>
+    <span class="ts">
+`;
 };
 
-hexo.extend.tag.register("docEditor", helper);
-hexo.extend.helper.register("docEditor", helper);
+hexo.extend.tag.register('docEditor', helper);
+hexo.extend.helper.register('docEditor', helper);
