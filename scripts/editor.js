36f272956e0d7d68738edeeb2f562f48c94a267e
Ekaterina Tokareva <Tokaree@labcorp.com>
2018-06-28 15:11:05 +0300

    format

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,8 +1,8 @@
 const helper = args => {
-  const name = args[0];
-  const file = "app/app.component.html";
-  const path = name.indexOf('github') >= 0 ? name : `edit/${name}`
-  return `
+	const name = args[ 0 ];
+	const file = "app/app.component.html";
+	const path = name.indexOf('github') >= 0 ? name : `edit/${name}`;
+	return `
     <span class="editor">
       <iframe data-src="//stackblitz.com/${path}?embed=1&file=${file}&hideNavigation=1&view=preview"></iframe>
     </span>
