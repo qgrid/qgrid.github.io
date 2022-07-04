31e8a8b4edae602b8e8c6b28853be5a9408277dc
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-07-04 16:32:37 +0300

    fix formatting of js and html

--- gulpfile.js
+++ gulpfile.js
@@ -9,53 +9,53 @@ const API_JSON_PATH = path.join(API_FOLDER, 'api.json');
 const API_TEMPLATE = fs.readFileSync(path.join('./api.hbs'), 'utf8');
 
 gulp.task('typedoc', () =>
-	gulp
-		.src(['./node_modules/ng2-qgrid/core/**/*.d.ts'])
-		.pipe(typedoc({
-			module: 'commonjs',
-			target: 'es5',
-			includeDeclarations: true,
-			json: API_JSON_PATH,
-			name: 'qgrid',
-			ignoreCompilerErrors: true,
-			excludeExternals: true
-		}))
+   gulp
+      .src(['./node_modules/ng2-qgrid/core/**/*.d.ts'])
+      .pipe(typedoc({
+         module: 'commonjs',
+         target: 'es5',
+         includeDeclarations: true,
+         json: API_JSON_PATH,
+         name: 'qgrid',
+         ignoreCompilerErrors: true,
+         excludeExternals: true
+      }))
 );
 
 gulp.task('markdown', done => {
-	const input = fs.readFileSync(API_JSON_PATH);
-	const project = JSON.parse(input);
-	project.children.forEach((unit, i) => {
-		if (!unit.children.some(x => x.comment && x.comment.shortText)) {
-			// omit files without documentaion
-			return;
-		}
+   const input = fs.readFileSync(API_JSON_PATH);
+   const project = JSON.parse(input);
+   project.children.forEach((unit, i) => {
+      if (!unit.children.some(x => x.comment && x.comment.shortText)) {
+         // omit files without documentaion
+         return;
+      }
 
-		unit.order = i;
+      unit.order = i;
 
-		const link = hbs.compile(API_TEMPLATE);
-		const output = link({ unit });
-		const name = path
-			.basename(unit.originalName)
-			.slice(0, -'.d.ts'.length)
-			.replace(/\./g, '-');
+      const link = hbs.compile(API_TEMPLATE);
+      const output = link({ unit });
+      const name = path
+         .basename(unit.originalName)
+         .slice(0, -'.d.ts'.length)
+         .replace(/\./g, '-');
 
-		console.log(`process: ${name}`);
+      console.log(`process: ${name}`);
 
-		fs.writeFileSync(
-			path.join(API_FOLDER, `${name}.md`),
-			output,
-			{ flag: 'w' }
-		);
+      fs.writeFileSync(
+         path.join(API_FOLDER, `${name}.md`),
+         output,
+         { flag: 'w' }
+      );
 
-		fs.writeFileSync(
-			path.join(API_FOLDER, `${name}.json`),
-			JSON.stringify(unit, null, 3),
-			{ flag: 'w' }
-		);
-	});
+      fs.writeFileSync(
+         path.join(API_FOLDER, `${name}.json`),
+         JSON.stringify(unit, null, 3),
+         { flag: 'w' }
+      );
+   });
 
-	done();
+   done();
 });
 
 gulp.task('api', gulp.series('typedoc', 'markdown'));
--- hbs.js
+++ hbs.js
@@ -2,71 +2,71 @@ const hbs = require('handlebars')
 const path = require('path');
 
 function isArray(x) {
-	return x.constructor === Array.prototype.constructor;
+   return x.constructor === Array.prototype.constructor;
 }
 
 function print(xs) {
-	if (isArray(xs)) {
-		return new hbs.SafeString(xs.join('\n'));
-	}
+   if (isArray(xs)) {
+      return new hbs.SafeString(xs.join('\n'));
+   }
 
-	return new hbs.SafeString(xs);
+   return new hbs.SafeString(xs);
 }
 
 function split(comment) {
-	const result = [];
-	if (comment) {
-		if (comment.shortText) {
-			result.push(comment.shortText);
-		}
+   const result = [];
+   if (comment) {
+      if (comment.shortText) {
+         result.push(comment.shortText);
+      }
 
-		if (comment.text) {
-			const lines = comment.text.split(/\r?\n/);
-			result.push(...lines);
-		}
-	}
-	return result;
+      if (comment.text) {
+         const lines = comment.text.split(/\r?\n/);
+         result.push(...lines);
+      }
+   }
+   return result;
 };
 
 hbs.registerHelper('api-title', unit => {
-	return print(unit.children.map(x => x.name).join(', '));
+   return print(unit.children.map(x => x.name).join(', '));
 });
 
 hbs.registerHelper('api-path', file => {
-	const name = path.basename(file).slice(0, -'.d.ts'.length).replace(/\./g, '-');
-	return print(`source/api/${name}.json`);
+   const name = path.basename(file).slice(0, -'.d.ts'.length).replace(/\./g, '-');
+   return print(`source/api/${name}.json`);
 });
 
 hbs.registerHelper('api-description', comment => {
-	const lines = split(comment);
-	const result = [];
-	for (const line of lines) {
-		if (line[0] === '#') {
-			break;
-		}
+   const lines = split(comment);
+   const result = [];
+   for (const line of lines) {
+      if (line[0] === '#') {
+         break;
+      }
 
-		result.push(line);
-	}
+      result.push(line);
+   }
 
-	return print(result);
+   return print(result);
 });
 
 
 hbs.registerHelper('api-comment', comment => {
-	const lines = split(comment);
-	const result = [];
-	let isComment = false;
-	for (const line of lines) {
-		if (!isComment && line[0] === '#') {
-			isComment = true;
-		}
+   const lines = split(comment);
+   const result = [];
+   let isComment = false;
+   for (const line of lines) {
+      if (!isComment && line[0] === '#') {
+         isComment = true;
+      }
 
-		if (isComment) {
-			result.push(line);
-		}
-	}
+      if (isComment) {
+         result.push(line);
+      }
+   }
 
-	return print(result);
+   return print(result);
 });
 
 hbs.registerHelper('api-order', unit => unit.order);
